a6af18d96d64a2a3b5d82202155b31cfad633046
Parnish Sharma <90691906+ParnishSharma@users.noreply.github.com>
2023-07-02 02:08:39 +0530

    added codes

---
+++ DAY2/lloops.java
@@ -0,0 +1,10 @@
+public class lloops {
+    public static void main(String[] args) {
+     
+        int n=0;
+       while(n < 2){
+        System.out.println("n");
+        n++;
+       }
+        }
+    }
