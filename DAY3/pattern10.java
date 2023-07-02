e42b7818c344bdc4b758aef61e8fc3eb92df33e0
Parnish Sharma <90691906+ParnishSharma@users.noreply.github.com>
2023-07-03 02:15:03 +0530

    add problems

---
+++ DAY3/pattern10.java
@@ -0,0 +1,14 @@
+public class pattern10 {
+        public static void main(String[] args){
+        
+        for(int i=1;i<=5;i++){
+            int k=1;
+            for (int j=i;j<=5;j++){
+                System.out.print(k);
+                k++;
+            }
+            
+            System.out.println("");
+        }
+    }
+}
