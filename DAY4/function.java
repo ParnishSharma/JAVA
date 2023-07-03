933823faa63e772a05ad6c4b19f2f28917f87672
Parnish Sharma <90691906+ParnishSharma@users.noreply.github.com>
2023-07-04 01:02:00 +0530

    one more

---
+++ DAY4/function.java
@@ -0,0 +1,17 @@
+package DAY4;
+import java.util.Scanner;
+
+public class function {
+    public static String myname() {
+        Scanner sc = new Scanner(System.in);
+        String num2 = sc.nextLine();
+        return num2;
+    }
+
+    public static void main(String[] args) {
+        String greet = "hey";
+        String result = myname();
+
+        System.out.println(greet+" "+result);
+    }
+}
