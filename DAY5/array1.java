61673c13064db370d6c4ae91e5e597edd5d12a7d
Parnish Sharma <90691906+ParnishSharma@users.noreply.github.com>
2023-07-07 00:54:44 +0530

    Create array1.java

---
+++ DAY5/array1.java
@@ -0,0 +1,18 @@
+package DAY5;
+
+public class array1 {
+    public static void main(String[] args){
+        int[] marks=new int[12];
+        marks[0]=91;
+         marks[1]=92;
+         marks[2]=92;
+         marks[3]=96;
+         marks[4]=95;
+         marks[5]=94;
+
+        for(int i=0;i<=5;i++){
+            System.out.println(marks[i]);
+        }
+    }
+    
+}
