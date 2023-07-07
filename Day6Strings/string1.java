763ed3103680a49c07b2fc0d63df299a19725952
Parnish Sharma <90691906+ParnishSharma@users.noreply.github.com>
2023-07-08 00:08:59 +0530

    Create string1.java

---
+++ Day6Strings/string1.java
@@ -0,0 +1,17 @@
+package Day6Strings;
+
+public class string1 {
+    public static void main(String args[]) {
+        int n=0;
+        String name = "tonymontanna";
+        for (int i = 0; i < name.length(); i++) {
+            if (name.charAt(i) == 'a') {
+                System.out.println(i);
+                n++;
+                
+            }
+        }
+                     System.out.println("Number of times it is coming "+n);
+   
+    }
+}
