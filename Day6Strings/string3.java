4cbbc03ab3fca5abcf49c30c4550f2311da55e49
Parnish Sharma <90691906+ParnishSharma@users.noreply.github.com>
2023-07-08 00:09:14 +0530

    Create string3.java

---
+++ Day6Strings/string3.java
@@ -0,0 +1,10 @@
+package Day6Strings;
+
+public class string3 {
+    public static void main(String args[]){
+        String name="tonny montanna";
+        String name2=name.substring(1 ,9);
+       System.out.println(name2);
+    }
+    
+}
