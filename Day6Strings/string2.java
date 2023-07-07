b689094e1bf033e91480d42fe3829f336f78e824
Parnish Sharma <90691906+ParnishSharma@users.noreply.github.com>
2023-07-08 00:09:06 +0530

    Create string2.java

---
+++ Day6Strings/string2.java
@@ -0,0 +1,21 @@
+package Day6Strings;
+
+public class string2 {
+    public static void main(String args[]) {
+        String name1 = "t";
+        String name2 = "t";
+
+        
+        //compareTo compares the alphabets too
+        // compareTo returns:
+        // positive value if name1 > name2
+        // 0 if name1 == name2
+        // negative value if name1 < name2
+
+        if (name1.compareTo(name2) == 0) {
+            System.out.println("Strings are equal");
+        } else {
+            System.out.println("Strings are not equal");
+        }
+    }
+}
