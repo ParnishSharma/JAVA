4babf2447dd6b967297ba83edd196adb08ddf472
Parnish Sharma <90691906+ParnishSharma@users.noreply.github.com>
2023-07-20 17:37:23 +0530

    Create towerofhanoi.java

---
+++ DAY8RECURSION/towerofhanoi.java
@@ -0,0 +1,28 @@
+package DAY8RECURSION;
+
+public class towerofhanoi {
+    public static void main(String[] args) {
+        int n = 2; // Number of disks
+        hanoi(n, "src", "h", "d"); // Call the hanoi function to solve the problem
+    }
+
+    public static void hanoi(int n, String source, String helper, String destination) {
+        if (n == 1) {
+            // Base case: If there is only one disk, move it from the source to the
+            // destination
+            System.out.println("Transfer disk from " + source + " to " + destination);
+            return;
+        }
+
+        // Move (n-1) disks from source to helper using destination as the auxiliary
+        // tower
+        hanoi(n - 1, source, destination, helper);
+
+        // Move the remaining largest disk from source to destination
+        System.out.println("Transfer disk from " + source + " to " + destination);
+
+        // Move (n-1) disks from helper to destination using source as the auxiliary
+        // tower
+        hanoi(n - 1, helper, source, destination);
+    }
+}
