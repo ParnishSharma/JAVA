4005ebc455163d3eeb47d6d919bda39dc5d60604
Parnish Sharma <90691906+ParnishSharma@users.noreply.github.com>
2023-07-20 15:07:35 +0530

    Create fibonaccisiries.java

---
+++ DAY8RECURSION/fibonaccisiries.java
@@ -0,0 +1,20 @@
+package DAY8RECURSION;
+
+public class fibonaccisiries {
+
+    public static void printfib(int x, int y, int n) {
+        if (n == 0) {
+            return;
+        }
+        int z = x + y;
+        System.out.print(z + " ");
+        printfib(y, z, n - 1);
+    }
+
+    public static void main(String args[]) {
+        int n = 7, a = 0, b = 1;
+        System.out.print(a + " ");
+        System.out.print(b + " ");
+        printfib(a, b, n - 2);
+    }
+}
