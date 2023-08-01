edcc8a1925c1c34bfbef3c892feade9d518350da
Parnish Sharma <90691906+ParnishSharma@users.noreply.github.com>
2023-08-02 00:14:19 +0530

    Create Polymorphism.java

---
+++ OOPSJAVA/Polymorphism.java
@@ -0,0 +1,33 @@
+package OOPSJAVA;
+
+class stud {
+    String name;
+    int roll;
+
+    public void printinfo(String name) {
+        System.out.println(name);
+    }
+
+    public void printinfo(int roll) {
+        System.out.println(roll);
+    }
+
+    public void printinfo(String name, int roll) {
+        System.out.println(name + " has " + roll);
+    }
+}
+
+public class Polymorphism {
+
+    public static void main(String args[]) {
+        stud s = new stud();
+        s.name = "anil";
+        s.roll = 22;
+
+        s.printinfo(s.name, s.roll);
+        s.printinfo(55);
+        s.printinfo("akshaj");
+
+    }
+}
+
