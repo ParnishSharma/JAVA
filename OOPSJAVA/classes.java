a46e7cc0a49eb8f8bce0f348106073e06599ffef
Parnish Sharma <90691906+ParnishSharma@users.noreply.github.com>
2023-10-24 14:28:30 +0530

    MORE COMMIT

---
+++ OOPSJAVA/classes.java
@@ -0,0 +1,16 @@
+package OOPSJAVA;
+
+public class classes {
+    public int x = 5;
+    static int y = 3;
+
+    public static void main(String[] args) {
+        classes myg = new classes();
+        System.out.println(myg.x);
+        System.out.println(myg.y);
+        System.out.println(x);
+        System.out.println(y);
+
+    }
+
+}
